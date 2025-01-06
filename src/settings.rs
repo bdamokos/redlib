a790693a4fb11fdb94062e5bb6c82a13393e041d
bdamokos <163609735+bdamokos@users.noreply.github.com>
2025-01-06 20:14:02 +0000

    Revert "try changing cookies to be domain aware"
    This reverts commit 2ec45edb82d3095a564d5cee80feccf849d4213b.

--- src/settings.rs
+++ src/settings.rs
@@ -85,21 +85,13 @@ pub async fn set(req: Request<Body>) -> Result<Response<Body>, String> {
 
 	for &name in &PREFS {
 		match form.get(name) {
-			Some(value) => {
-				let mut cookie = Cookie::build((name.to_owned(), value.clone()))
+			Some(value) => response.insert_cookie(
+				Cookie::build((name.to_owned(), value.clone()))
 					.path("/")
 					.http_only(true)
-					.expires(OffsetDateTime::now_utc() + Duration::weeks(52));
-				
-				// Add domain from request host header
-				if let Some(host) = req.headers().get("host") {
-					if let Ok(host_str) = host.to_str() {
-						cookie = cookie.domain(host_str.to_owned());
-					}
-				}
-				
-				response.insert_cookie(cookie.into());
-			}
+					.expires(OffsetDateTime::now_utc() + Duration::weeks(52))
+					.into(),
+			),
 			None => response.remove_cookie(name.to_string()),
 		};
 	}
@@ -132,21 +124,13 @@ fn set_cookies_method(req: Request<Body>, remove_cookies: bool) -> Response<Body
 
 	for name in [PREFS.to_vec(), vec!["subscriptions", "filters"]].concat() {
 		match form.get(name) {
-			Some(value) => {
-				let mut cookie = Cookie::build((name.to_owned(), value.clone()))
+			Some(value) => response.insert_cookie(
+				Cookie::build((name.to_owned(), value.clone()))
 					.path("/")
 					.http_only(true)
-					.expires(OffsetDateTime::now_utc() + Duration::weeks(52));
-				
-				// Add domain from request host header
-				if let Some(host) = req.headers().get("host") {
-					if let Ok(host_str) = host.to_str() {
-						cookie = cookie.domain(host_str.to_owned());
-					}
-				}
-				
-				response.insert_cookie(cookie.into());
-			}
+					.expires(OffsetDateTime::now_utc() + Duration::weeks(52))
+					.into(),
+			),
 			None => {
 				if remove_cookies {
 					response.remove_cookie(name.to_string());
